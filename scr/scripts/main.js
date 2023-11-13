67bfa3afc5a4f01d1a0d1629a859061fca379779
SalocinBRs <nv15983@gmail.com>
2023-11-13 19:57:48 -0300

    v0.85
    implementado dificuldade progressiva

--- scr/scripts/main.js
+++ scr/scripts/main.js
@@ -12,7 +12,8 @@ const state = {
         countDownTimerId: setInterval(countDown, 800),
         hitPosition: 0,
         result: 0,
-        curretTime: 15,
+        currentTime: 20, // Renomeado para currentTime para manter a consistência
+        initialInterval: 1000, // Adicionado o intervalo inicial
     },
 };
 
@@ -27,20 +28,19 @@ function randomSquare() {
     state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, 1000);
+function moveEnemy(intervalo = state.values.initialInterval) {
+    state.values.timerId = setInterval(randomSquare, intervalo);
 }
 
 function countDown() {
-    state.values.curretTime--;
-    state.view.timeLeft.textContent = state.values.curretTime;
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
     
-    if (state.values.curretTime <= 0) {
-        clearInterval(state.values.countDownTimerId)
-        clearInterval(state.values.timerId)
-        playSound("death.m4a", 1)
-        alert('Gamer over, o seu resultado foi: ' + state.values.result)
-        
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.values.countDownTimerId);
+        clearInterval(state.values.timerId);
+        playSound("death.m4a", 1);
+        alert('Gamer over, o seu resultado foi: ' + state.values.result);
     }
 }
 
@@ -48,34 +48,40 @@ function playSound(soundName, volume) {
     let audio = new Audio(`./scr/sounds/${soundName}`)
     audio.volume = volume;
     audio.play();
-
 }
+
 function addListerHitBox() {
     state.view.square.forEach((square) => {
         square.addEventListener("mousedown", () => {
             if (square.id === state.values.hitPosition) {
-                state.values.result++
+                state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
                 playSound("hit.m4a", 0.08);
-                state.values.curretTime += 2 ;
+                state.values.currentTime += 2;
+
+                // Diminui 25 milissegundos do intervalo a cada clique bem-sucedido
+                state.values.initialInterval -= 50;
+
+                // Limpa o intervalo atual
+                clearInterval(state.values.timerId);
+
+                // Inicia um novo intervalo com o valor atualizado
+                moveEnemy(state.values.initialInterval);
             }
-            
-        })
+        });
     });
 }
-
 
 function initialize() {
     playSound("stageSound.m4a", 1)
     randomSquare();
-    moveEnemy()
-    addListerHitBox()
+    moveEnemy();
+    addListerHitBox();
 }
 
 state.view.reset.addEventListener('click', function() {
-    window.location.reload()
-    
-})
-  
+    window.location.reload();
+});
+
 initialize();
